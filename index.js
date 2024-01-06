8824259f6891d1ed90c36491929518afe0c70eed
Vineeth1512 <114067110+Vineeth1512@users.noreply.github.com>
2024-01-06 23:10:17 +0530

    added some files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const portNo = 5678
 
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views')); 
 app.use(express.static("public"));
 app.get("/", (req, res) => {
     return res.render("home");
